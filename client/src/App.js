076758fe0983ced212ef981268011792fa9b9a03
PAVAN KUMAR P S <pavanscienti2001@gmail.com>
2023-09-25 16:57:26 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom'
+
+import Home from "./scenes/home/Home"
+import itemDetails from './scenes/itemDetails/itemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
+import Navbar from './scenes/global/Navbar';
+import CartMenu from './scenes/global/CartMenu';
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname])
+  return null
+}
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/item/:itemId' element={<itemDetails />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/checkout/success' element={<Confirmation />} />
+        </Routes>
+        <CartMenu />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/scenes/itemDetails/itemDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const itemDetails = () => {
+  return (
+    <div>
+      itemDetails
+    </div>
+  )
+}
+
+export default itemDetails
